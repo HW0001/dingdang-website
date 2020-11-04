1a0ceb2e7ba9e524aca1165012bb6aa765d8ff21
HW0001 <1600860624@qq.com>
2020-11-04 19:13:17 +0800

    deploy

---
+++ precache-manifest.2096a9132bcd01a172b9ba40759f7580.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cb94286182936465d1f",
+    "url": "/dingdang-website/css/app.64954224.css"
+  },
+  {
+    "revision": "bf53bccca2471925d1cf",
+    "url": "/dingdang-website/css/chunk-vendors.6dc57fe0.css"
+  },
+  {
+    "revision": "7a20304b0e30902996dbbd148dbc3724",
+    "url": "/dingdang-website/index.html"
+  },
+  {
+    "revision": "3cb94286182936465d1f",
+    "url": "/dingdang-website/js/app.4fa77bc4.js"
+  },
+  {
+    "revision": "bf53bccca2471925d1cf",
+    "url": "/dingdang-website/js/chunk-vendors.26e9bfa9.js"
+  },
+  {
+    "revision": "0581bc80abf348c4290af7faf8d4e572",
+    "url": "/dingdang-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dingdang-website/robots.txt"
+  }
+]);
